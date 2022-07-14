c706aed1341fcde80650dbd7784b53a7fc57a47c
luancs <luan.soares.ext@infracommerce.com.br>
2022-07-14 14:53:10 -0300

    feature: Created home page elements and styles.

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,5 +1,32 @@
+import { Link } from 'react-router-dom';
+
+import styles from './Home.module.scss';
+
 function Home() {
-  return <div>Home</div>;
+  return (
+    <main className={styles.container}>
+      <header>
+        <img src="images/logo.png" alt="Flexge logo" />
+      </header>
+      <div className={styles.loginContainer}>
+        <p>Sign in with your Flexge account</p>
+
+        <form method="POST">
+          <div className={styles.inputContainer}>
+            <input type="email" placeholder="E-mail" required />
+          </div>
+          <div className={styles.inputContainer}>
+            <input type="password" placeholder="Password" required />
+          </div>
+
+          <div className={styles.actions}>
+            <Link to="/forget">Forgot your password?</Link>
+            <button type="submit">Sign In</button>
+          </div>
+        </form>
+      </div>
+    </main>
+  );
 }
 
 export { Home };
