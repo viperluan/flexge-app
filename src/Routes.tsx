655b39d3d1cf696daa7de7c83c04de0dcee32abc
luancs <luan.soares.ext@infracommerce.com.br>
2022-07-16 19:54:31 -0300

    feature: Added newStudents page on routes.

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { Home } from './pages/Home.page';
+import { NewStudents } from './pages/new-students/NewStudents.page';
 import { Students } from './pages/students/Students.page';
 
 function NotFound() {
@@ -13,6 +14,8 @@ function AppRoutes() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/students" element={<Students />} />
+        <Route path="/students/new" element={<NewStudents />} />
+
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
