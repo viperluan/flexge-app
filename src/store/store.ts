b488d35d8fbc0b5c26e20cea96ad6ccaf1469db4
luancs <luan.soares.ext@infracommerce.com.br>
2022-07-18 02:25:08 -0300

    feature: Imported student slice on store.

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import authenticateReducer from './authenticateSlice';
+import studentsReducer from './studentsSlice';
 
 export default configureStore({
   reducer: {
     authenticate: authenticateReducer,
+    students: studentsReducer,
   },
 });
