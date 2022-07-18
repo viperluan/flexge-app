240e4f0b14b9369c8b661239230148b947ac733a
luancs <luan.soares.ext@infracommerce.com.br>
2022-07-18 13:29:11 -0300

    feature: Implemented logic to use cookies of authenticate.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
+import { useEffect } from 'react';
+import { useCookies } from 'react-cookie';
+import { useDispatch } from 'react-redux';
+
 import { AppRoutes } from './Routes';
+import { login } from './store/authenticateSlice';
 
 function App() {
+  const dispatch = useDispatch();
+  const [cookies, setCookie, removeCookie] = useCookies();
+  const { token, email } = cookies;
+
+  useEffect(() => {
+    if (token) {
+      dispatch(login({ token, email }));
+    }
+  }, []);
+
   return <AppRoutes />;
 }
 
