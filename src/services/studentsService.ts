3ba2aa478452886babdd49e548116a98e926be9d
luancs <luan.soares.ext@infracommerce.com.br>
2022-07-18 18:04:36 -0300

    feature: Created createStudent and editStudent methods.

--- src/services/studentsService.ts
+++ src/services/studentsService.ts
@@ -1,4 +1,13 @@
+/* eslint-disable no-underscore-dangle */
 import api from './api';
+
+interface IStudentData {
+  _id?: string | number;
+  name: string;
+  age: number;
+  course: string;
+  school: string;
+}
 
 const getStudents = async () => {
   const response = await api.get('/students');
@@ -6,4 +15,16 @@ const getStudents = async () => {
   return response;
 };
 
-export { getStudents };
+const createStudent = async (student: IStudentData) => {
+  const response = await api.post('/students', student);
+
+  return response;
+};
+
+const editStudent = async (student: IStudentData) => {
+  const response = await api.put(`/students/${student._id}`, student);
+
+  return response;
+};
+
+export { getStudents, createStudent, editStudent };
