f5c9d61652b1b41e2db787237baefcc5bd9c79f9
luancs <luan.soares.ext@infracommerce.com.br>
2022-07-18 01:33:56 -0300

    feature: Create authenticate slice.

---
+++ src/store/authenticateSlice.ts
@@ -0,0 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const INITIAL_STATE = { email: '', password: '', token: '' };
+
+export const authenticateSlice = createSlice({
+  name: 'authenticate',
+  initialState: INITIAL_STATE,
+  reducers: {
+    login: (state, action) => {
+      if (action.type === 'authenticate/login') {
+        const { payload } = action;
+        return payload;
+      }
+
+      return state;
+    },
+    logout: () => INITIAL_STATE,
+  },
+});
+
+export const { login, logout } = authenticateSlice.actions;
+
+export default authenticateSlice.reducer;
