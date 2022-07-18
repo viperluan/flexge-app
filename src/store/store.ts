e3fa966bb581a05f6ae91e18f6e6e6db7b3d8bd6
luancs <luan.soares.ext@infracommerce.com.br>
2022-07-18 01:34:15 -0300

    feature: Import authenticate reducer.

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 
+import authenticateReducer from './authenticateSlice';
+
 export default configureStore({
-  reducer: {},
+  reducer: {
+    authenticate: authenticateReducer,
+  },
 });
